c3592aa951336f5c0d0814ff8e765ba3ed55d6ce
Joko Irianto <irianto94@outlook.com>
2019-09-10 05:54:54 +0700

    basic graphql work

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello world!');
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            users: [String!]!
+        }
+
+        type RootMutation {
+            createUser(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        users: () => {
+            return ['A', 'B', 'C'];
+        },
+        createUser: (args) => {
+            const userName = args.name;
+            return userName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
 
